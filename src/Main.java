2e9bb45738b0c0fbf5ace68356e4d0a29d2d4663
Nikita <nikitabarkovskij44@gmail.com>
2023-11-07 17:05:24 +0300

    update code

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        int year;
+
+        System.out.println("Программа позволяет узнать в какой день отмичается день программиста ");
+
+        Scanner scanner = new Scanner(System.in);
+        year = scanner.nextInt();
+
+        if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0)
+            System.out.println("12/09/" + year);
+        else
+            System.out.println("13/09/" + year);
+
     }
 }
